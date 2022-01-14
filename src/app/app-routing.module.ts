28a692d47e10f4c07f44c9423864c73b685644b3
misuvii <midhun.m@neoito.com>
2022-01-14 16:56:14 +0530

    fix: routing fix and auth layout setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RoutesConfig } from './configs/routes.config';
+import { AuthGuard } from './modules/auth/auth.guard';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./modules/root/root.module').then(m => m.RootModule) },
+  {
+    path: RoutesConfig.basePaths.auth,
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path: '',
+    loadChildren: () => import('./modules/root/root.module').then(m => m.RootModule),
+  },
 ];
 
 @NgModule({
--- src/app/modules/root/root.guard.ts
+++ src/app/modules/root/root.guard.ts
@@ -1,20 +1,18 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
-import { AuthService } from './auth.service';
-import { RoutesConfig } from '../../configs/routes.config';
+import { AuthService } from '../auth/auth.service';
 
-const authRoutes = RoutesConfig.routesNames.auth;
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class RootGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
     return new Promise(resolve => {
-      if (this.authService.isLoggedIn()) {
+      if (!this.authService.isLoggedIn()) {
         // check wheather a user is login or not
         resolve(true);
       } else {
-        this.router.navigate([RoutesConfig.routes.auth.logIn]);
+        this.router.navigate(['']);
         resolve(false);
       }
     });
