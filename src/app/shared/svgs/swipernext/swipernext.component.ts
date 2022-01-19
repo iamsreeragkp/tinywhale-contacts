a96ee5d1a336fbda69dd15feb3f30bb40e727c22
Manjuneoito <manju@neoito.com>
2022-01-19 20:43:50 +0530

    Style: Forgotpasswordotp

---
+++ src/app/shared/svgs/swipernext/swipernext.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-swipernext',
+  templateUrl: './swipernext.component.svg',
+  styleUrls: ['./swipernext.component.css']
+})
+export class SwipernextComponent {
+  
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
