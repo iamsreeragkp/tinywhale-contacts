408cbad383691930f358046ff3f3581f12b683fb
misuvii <67205563+misuvii@users.noreply.github.com>
2022-01-20 20:01:50 +0530

    fix: added missed import website component (#27)

--- src/app/modules/website/website.module.ts
+++ src/app/modules/website/website.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WebsiteRoutingModule } from './website-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { WebsiteComponent } from './website.component';
 
 @NgModule({
   imports: [CommonModule, WebsiteRoutingModule, SharedModule],
-  declarations: [],
+  declarations: [WebsiteComponent],
   providers: [],
 })
 export class WebsiteModule {}
