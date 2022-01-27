635fdd6fc2b5ff294168552b0485459588cafbb2
Manjuneoito <manju@neoito.com>
2022-01-27 19:41:54 +0530

    swiper UI issue Fixed

--- src/app/modules/auth/auth.component.ts
+++ src/app/modules/auth/auth.component.ts
@@ -1,8 +1,7 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { transition, trigger, useAnimation } from '@angular/animations';
 import { fadeIn } from 'ng-animate';
 import Swiper, { Navigation, Pagination } from 'swiper';
-import { SwiperComponent } from 'swiper/angular';
 
 @Component({
   selector: 'app-auth',
@@ -19,29 +18,17 @@ import { SwiperComponent } from 'swiper/angular';
     ]),
   ],
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent {
   slides = [1, 2, 3];
   @ViewChild('newSwiper') newSwiper: any;
-  @ViewChild('swiperImg') swiperImg: any;
   constructor() {
     Swiper.use([Navigation, Pagination]);
   }
-  ngOnInit() {
-    setTimeout(function () {
-      var swiper = new Swiper('.swiper-container');
-    }, 500);
-    console.log(this.swiperImg);
-  }
+
   swipePrev() {
-    console.log(this.swiperImg);
     this.newSwiper.swiperRef.slidePrev();
   }
   swipeNext() {
-    console.log(this.swiperImg);
     this.newSwiper.swiperRef.slideNext();
-  }
-  get relativeHeight(){
-    return this.swiperImg?.nativeElement?.clientHeight-20;
-    
   }
 }
