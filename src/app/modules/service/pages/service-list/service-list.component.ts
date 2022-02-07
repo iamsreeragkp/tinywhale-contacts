64bdb50b607e3795ac708ac3caaa82e5e0668fc7
naveen <naveen@neoito.com>
2022-02-07 20:26:22 +0530

    refactor: add service button logic

--- src/app/modules/service/pages/service-list/service-list.component.ts
+++ src/app/modules/service/pages/service-list/service-list.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Observable, Subject, takeUntil } from 'rxjs';
+import { select, Store } from '@ngrx/store';
+import { getDashboardData } from '../../../root/store/root.selectors';
+import { IRootState } from 'src/app/modules/root/store/root.reducers';
 
 @Component({
   selector: 'app-service-list',
@@ -6,5 +10,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./service-list.component.scss'],
 })
 export class ServiceListComponent {
-  constructor() {}
+
+  dashboard$: Observable<any>;
+  ngUnsubscriber = new Subject<void>();
+  dashboardInfos: any = undefined;
+  constructor(private store: Store<IRootState>) {
+    this.dashboard$ = store.pipe(select(getDashboardData));
+  }
+
+  ngOnInit(): void {
+    this.subscriptions();
+  }
+
+  subscriptions() {
+    this.dashboard$.pipe(takeUntil(this.ngUnsubscriber)).subscribe(data => {
+      this.dashboardInfos = data; 
+    }, err=> {
+      console.log("ERROR OCCURED",err);
+      
+    });
+    
+  }
+
+  hasStarted()
+  {                                                                                       
+    return ( this.dashboardInfos?.businessInfo?.isStarted || this.dashboardInfos?.serviceInfo?.isStarted || this.dashboardInfos?.paymentInfo?.isStarted)
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscriber.next();
+    this.ngUnsubscriber.complete();
+  }
 }
